4502abc40c5b90f1b662f3adadefd69abf50d4c6
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-09-22 21:25:23 -0500

    ProductList.jsx and ProductList.scss were added

---
+++ src/containers/ProductList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProductItem from '../components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
+import '@styles/ProductList.scss';
+
+
+const ProductList = () => {
+	const products = useGetProducts();
+	return (
+    <main className="main-container">
+      <section className="ProductList">
+				{products.map(product => (
+					<ProductItem product={product} key={product.id} />
+				))}
+			</section>
+		</main>
+	);
+}
+
+export default ProductList;
