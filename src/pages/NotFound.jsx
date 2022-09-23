3129518d87130b925005b7c759ed0c8c040420ee
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-09-22 22:05:24 -0500

    NotFound.jsx and NotFound.scss were added

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import '../styles/NotFound.scss'
+
+const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <h1>Error 404</h1>
+      <h1 id="error">Page Not Found</h1>
+    </div>
+  );
+}
+
+export default NotFound;
