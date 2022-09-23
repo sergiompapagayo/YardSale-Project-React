bf34cec2c72a1e7a4be3180776028b81b2d5663b
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-09-22 22:20:20 -0500

    .babelrc and webpack.config.js were updated

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), //dist de distribution
     filename: 'bundle.js',
+    publicPath: '/'
   },
   mode: 'development',
   resolve: {
